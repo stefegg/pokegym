a4033b367e9866e213f31a4d1173205350435880
Karl Rodulfo <karlrodulfo@gmail.com>
2018-07-26 00:39:46 -0400

    so many styling changes :

---
+++ banner.js
@@ -0,0 +1,33 @@
+function enterGym() {
+document.getElementById("enter").classList.remove("bannerbefore");
+document.getElementById("enter").classList.add("bannerafter");
+document.getElementById("enterButton").classList.add("hidden2");
+}
+
+// window.addEventListener("scroll", enterGym());
+
+var last_known_scroll_position = 0;
+var ticking = false;
+scroll_pos = last_known_scroll_position;
+
+// if (scroll_pos >= 0) {
+//   enterGym();}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      enterGym()
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+
+});
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
