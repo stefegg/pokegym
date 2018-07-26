0d947728100005d276649cfc924b36522aa3d0cd
Stef Egbert <stephenegbert@gmail.com>
2018-07-26 11:09:38 -0400

    have to re add banner.js

--- application.js
+++ application.js
@@ -2,41 +2,6 @@ var last_known_scroll_position = 0;
 var ticking = false;
 // var ticking = true;
 
-function enterGym() {
-  document.getElementById("enter").classList.remove("bannerbefore");
-  document.getElementById("enter").classList.add("bannerafter");
-  document.getElementById("enterButton").classList.add("hidden2");
-}
-
-// window.addEventListener("scroll", enterGym());
-
-var last_known_scroll_position = 0;
-var ticking = false;
-scroll_pos = last_known_scroll_position;
-
-// if (scroll_pos >= 0) {
-//   enterGym();}
-
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
-
-    window.requestAnimationFrame(function() {
-      enterGym()
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-
-});
-
-//this forces reload at start! weee
-
-window.onbeforeunload = function() {
-  window.scrollTo(0, 0);
-}
 
 trainerTaunt = {
   sT: 'Hi! I like shorts! They’re comfy and easy to wear! Whenever I’m feeling down after losing a battle, I just think, ‘At least I’ve still got my shorts!',
